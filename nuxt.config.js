dc34792eded2a44e1949ba39675717c83c46b2e8
dev-les <luisenriquesan97@gmail.com>
2022-05-15 21:04:14 -0400

    updated deploy action

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,15 +21,6 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '~assets/css/main.css'
-  ],
-  styleResources: {
-    scss: [
-      '~assets/scss/mixins.scss',
-      '~assets/scss/variables.scss'
-    ]
-  },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
