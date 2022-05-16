bbb5a8e3de924bc5c56dbf0ddd6ee781e687378c
dev-les <luisenriquesan97@gmail.com>
2022-05-15 21:44:17 -0400

    using this branch as deploy branch

---
+++ dist/_nuxt/9815701.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{313:function(e,n,t){"use strict";t.r(n);var c=t(48),component=Object(c.a)({},(function(){var e=this,n=e.$createElement;return(e._self._c||n)("h1",[e._v("Projects")])}),[],!1,null,null,null);n.default=component.exports}}]);
---
+++ dist/_nuxt/static/1652662897/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"web\u002F_nuxt\u002Fstatic\u002F1652662897",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"web\u002F",assetsPath:"web\u002F_nuxt\u002F",cdnURL:a}}}}(null));
