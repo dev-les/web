b17a8c3e7c2405edc5879cfc064112e198059d51
dev-les <luisenriquesan97@gmail.com>
2022-05-15 22:04:37 -0400

    moved everything out of dist folder

--- _nuxt/static/1652662897/blog/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"web\u002F_nuxt\u002Fstatic\u002F1652662897",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog",config:{_app:{basePath:"web\u002F",assetsPath:"web\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- _nuxt/static/1652662897/links/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"web\u002F_nuxt\u002Fstatic\u002F1652662897",layout:"links",error:a,serverRendered:true,routePath:"\u002Flinks",config:{_app:{basePath:"web\u002F",assetsPath:"web\u002F_nuxt\u002F",cdnURL:a}}}}(null));
